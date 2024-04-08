a26c97bad458b66565b45674ea750bb882afeb43
길근우 <kesksjxhx@gmail.com>
2024-04-08 11:41:39 +0900

    🧩 :: contentDialog 보기

--- app/src/main/java/com/geunoo/android_lab/feature/shortbooks/ShortBooksScreen.kt
+++ app/src/main/java/com/geunoo/android_lab/feature/shortbooks/ShortBooksScreen.kt
@@ -1,18 +1,24 @@
 package com.geunoo.android_lab.feature.shortbooks
 
+import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
@@ -22,76 +28,143 @@ import com.geunoo.android_lab.R
 import com.geunoo.android_lab.ui.component.Header
 import team.returm.jobisdesignsystemv2.foundation.JobisTheme
 import team.returm.jobisdesignsystemv2.foundation.JobisTypography
+import team.returm.jobisdesignsystemv2.utils.clickable
 
 @Composable
 fun ShortBooksScreen() {
-    Column(
-        modifier = Modifier
-            .fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-    ) {
-        Header()
-        Image(
+    val (showDialog, setShowDialog) = remember { mutableStateOf(false) }
+    Box {
+        Column(
             modifier = Modifier
-                .padding(
-                    top = 8.dp,
-                    start = 26.dp,
-                    end = 26.dp,
-                )
-                .shadow(10.dp),
-            painter = painterResource(R.drawable.ic_book),
-            contentDescription = "임시",
-        )
-        Text(
-            modifier = Modifier.padding(top = 16.dp, bottom = 16.dp),
-            text = "눌러서 내용 보기",
-            color = JobisTheme.colors.onSurface,
-        )
-        Row(
-            modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(
-                            Color.White,
-                            JobisTheme.colors.onSurfaceVariant,
-                        )
-                    )
+                .fillMaxSize()
+                .clickable(
+                    enabled = true,
+                    onPressed = {},
+                    onClick = { setShowDialog(true) },
                 ),
-            horizontalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Column {
-                Text(
-                    modifier = Modifier
-                        .padding(
-                            start = 12.dp,
-                            top = 18.dp,
-                        ),
-                    text = "우리는 여전히 삶을 사랑하는가",
-                    style = JobisTypography.HeadLine,
-                    color = JobisTheme.colors.onBackground,
-                )
-                Text(
-                    modifier = Modifier
-                        .padding(
-                            top = 8.dp,
-                            start = 12.dp,
-                        ),
-                    text = "에리히 프롬",
-                    style = JobisTypography.Body,
-                )
-            }
-            Icon(
+            Header()
+            Image(
                 modifier = Modifier
                     .padding(
-                        start = 80.dp,
-                        end = 18.dp,
-                        top = 18.dp,
+                        top = 8.dp,
+                        start = 26.dp,
+                        end = 26.dp,
+                    )
+                    .shadow(10.dp),
+                painter = painterResource(R.drawable.ic_book),
+                contentDescription = "임시",
+            )
+            Text(
+                modifier = Modifier
+                    .padding(
+                        top = 16.dp,
+                        bottom = 16.dp,
+                    )
+                    .clickable(
+                        enabled = true,
+                        onPressed = {},
+                        onClick = { setShowDialog(true) },
                     ),
-                painter = painterResource(id = R.drawable.ic_like_false),
-                contentDescription = ""
+                text = "눌러서 내용 보기",
+                color = JobisTheme.colors.onSurface,
+            )
+            Row(
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
+                    .background(
+                        brush = Brush.verticalGradient(
+                            colors = listOf(
+                                Color.White,
+                                JobisTheme.colors.onSurfaceVariant,
+                            )
+                        )
+                    ),
+                horizontalArrangement = Arrangement.SpaceBetween,
+            ) {
+                Column {
+                    Text(
+                        modifier = Modifier
+                            .padding(
+                                start = 12.dp,
+                                top = 18.dp,
+                            ),
+                        text = "우리는 여전히 삶을 사랑하는가",
+                        style = JobisTypography.HeadLine,
+                        color = JobisTheme.colors.onBackground,
+                    )
+                    Text(
+                        modifier = Modifier
+                            .padding(
+                                top = 8.dp,
+                                start = 12.dp,
+                            ),
+                        text = "에리히 프롬",
+                        style = JobisTypography.Body,
+                    )
+                }
+                Icon(
+                    modifier = Modifier
+                        .padding(
+                            start = 80.dp,
+                            end = 18.dp,
+                            top = 18.dp,
+                        ),
+                    painter = painterResource(id = R.drawable.ic_like_false),
+                    contentDescription = ""
+                )
+            }
+        }
+        if (showDialog) {
+            ContentDialog(
+                content = "dajldskajdklsajdklsadjklsadjkasljdklasjdkalsjdklajdkldasjkljsakljdkl",
+                hideDialog = {
+                    setShowDialog(false)
+                    Log.d("TEST", showDialog.toString())
+                }
             )
         }
+    }
+}
+
+@Composable
+fun ContentDialog(
+    content: String,
+    hideDialog: () -> Unit,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .alpha(0.8f)
+            .background(Color.Black)
+            .clickable(
+                enabled = true,
+                onPressed = {},
+                onClick = hideDialog,
+            )
+            .padding(
+                horizontal = 34.dp,
+                vertical = 18.dp,
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text(
+            text = content,
+            color = JobisTheme.colors.background,
+            style = JobisTypography.Body,
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Text(
+            modifier = Modifier.clickable(
+                enabled = true,
+                onPressed = {},
+                onClick = hideDialog,
+            ),
+            text = "눌러서 돌아가기",
+            style = JobisTypography.Caption,
+            color = JobisTheme.colors.surfaceTint,
+        )
     }
 }
